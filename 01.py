c165e1833dc770d15ab42895d14424a37996f029
ramya <ramya123@gmail.com>
2026-02-22 11:24:42 +0530

    first program changed

--- 01.py
+++ 01.py
@@ -1,5 +1,7 @@
 a=int(input("Enter a number: "))
 b=int(input("Enter another number: "))
+c=a+b
+print("The sum of the two numbers is:", c)
 if a>b:
     print("The first number is greater than the second number.")
 elif a<b:
